9d08fadb474930aa9683e8d0e8e6c333ee7cfd2a
Akhil K <akhilkponnanivj45@gmail.com>
2021-05-24 16:46:58 +0530

    Edit product

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -8,15 +8,34 @@ module.exports = {
       callback(data.ops[0]._id);
     })
   },
-  viewProducts:function(){
+  viewProducts:function() {
     return new Promise(async(resolve, reject) => {
       let products = await db.get().collection(collections.PRODUCT_COLLECTION).find().toArray();
       resolve(products);
     })
   },
-  deleteProduct:function(productId){
+  deleteProduct:function(productId) {
     return new Promise((resolve, reject) => {
       db.get().collection(collections.PRODUCT_COLLECTION).removeOne({_id:objectId(productId)}).then((response) => {
+        resolve(response)
+      })
+    })
+  },
+  getProductDetails:function(productId) {
+    return new Promise((resolve, reject) => {
+      db.get().collection(collections.PRODUCT_COLLECTION).findOne({_id:objectId(productId)}).then((product) => {
+        resolve(product)
+      })
+    })
+  },
+  updateProduct:function(productId, productDetails) {
+    return new Promise((resolve, reject) => {
+      db.get().collection(collections.PRODUCT_COLLECTION).updateOne({_id:objectId(productId)}, {$set:{
+        name:productDetails.name,
+        category:productDetails.category, 
+        description:productDetails.description,
+        price:productDetails.price
+      }}).then((response) => {
         resolve(response)
       })
     })
--- routes/admin.js
+++ routes/admin.js
@@ -20,7 +20,7 @@ router.post('/add-product', function(req, res, next) {
       if(!err)
         res.render('admin/add-product');
       else
-        console.log(err)
+        console.log(err);
     });
   });
 });
@@ -28,8 +28,25 @@ router.post('/add-product', function(req, res, next) {
 router.get('/delete-product/:id', function(req, res, next) {
   productId = req.params.id
   productHelpers.deleteProduct(productId).then((response) => {
-    res.redirect('/admin')
+    res.redirect('/admin');
   })
 });
+
+router.get('/edit-product/:id', async function(req, res, next) {
+  let product = await productHelpers.getProductDetails(req.params.id)
+  res.render('admin/edit-product', {product, admin:true});
+});
+
+router.post('/edit-product/:id', function(req, res, next) {
+  let id = req.params.id;
+  productHelpers.updateProduct(req.params.id, req.body).then(() =>{
+    res.redirect('/admin')
+    if(req.files.image)
+    {
+      let image = req.files.image;
+      image.mv('./public/product-images/'+id+'.jpg');
+    }
+  })
+})
 
 module.exports = router;
